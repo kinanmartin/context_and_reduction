a4ba198fd725e89018041ccea9a6b0106c87308d
Kinan Martin <kinanmartin0@gmail.com>
2024-04-23 18:08:58 -0400

    train_models.py v0 no testing yet

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -1,35 +1,86 @@
-from tqdm import tqdm
+# from tqdm import tqdm
 # from tqdm.notebook import tqdm
 from datasets import load_dataset, DatasetDict, load_from_disk
-from transformers import GPT2Tokenizer, GPT2Config, GPT2LMHeadModel, GPT2LMHeadModel
-from transformers import DataCollatorForLanguageModeling
+# from transformers import GPT2Tokenizer, GPT2Config, GPT2LMHeadModel, GPT2LMHeadModel
+# from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
-import random
+import accelerate
+import transformers
+# transformers.__version__, accelerate.__version__
+import torch
+torch.cuda.is_available()
+from utils import *
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(torch.cuda.get_device_name(0))
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("input_data_path")
-    parser.add_argument("tokenized_data_path")
-    parser.add_argument("need_to_tokenize")
-    parser.add_argument("model_output_path")
-
-    parser.set_defaults(
-
-    )
+    # parser.add_argument("--input_data_dir")
+    parser.add_argument("--tokenized_data_dir")
+    parser.add_argument("--model_dir")
+    parser.add_argument("--eval", default=False)
+    parser.add_argument("--from_pretrained", default=None)
+    # parser.add_argument("--need_to_tokenize")
+    parser.add_argument("--context_size", default=None)
+    parser.add_argument("--context_direction", default='left')
 
     args = parser.parse_args()
 
-    input_data_path = args.input_data_path
-    tokenized_data_path = args.tokenized_data_path
-    need_to_tokenize = args.need_to_tokenize
+    # Init/load model
+    if args.from_pretrained:
+        model = load_pretrained_model(args.from_pretrained)
+    else:
+        model = init_model(args.context_size)
 
-    # Initialize tokenizer
-    context_length = 1024
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model.to(device)
+    if args.eval:
+        model.eval()
 
-    # Load (pre)tokenized data
-    # TOKENIZE = sys.argv[0]
-    # if TOKENIZE: 
-    #     pass
+    # Load tokenizer
+    tokenizer = load_pretrained_tokenizer(args.context)
+
+    # Create data collator
+    data_collator = init_data_collator(args.context_direction)
+
+    args = TrainingArguments(
+        args.model_dir,
+        per_device_train_batch_size=32, # change to fit GPU specs
+        per_device_eval_batch_size=32,
+        evaluation_strategy='epoch',
+        eval_steps=0.25,
+        logging_steps=0.25,
+        save_strategy='epoch',
+        save_steps=0.25,
+        group_by_length=True, # bucketing
+        load_best_model_at_end=True,
+        metric_for_best_model='loss',
+        greater_is_better=False,
+        save_total_limit=3,
+        num_train_epochs=1,
+    )
+
+    tokenized_dataset_dict = load_from_disk(args.tokenized_data_dir)
+    tokenized_dataset_dict = tokenized_dataset_dict.remove_columns(['text'])
+
+    if not args.eval:
+        trainer = Trainer(
+            model=model,
+            tokenizer=tokenizer,
+            args=args,
+            data_collator=data_collator,
+            train_dataset=tokenized_dataset_dict['train'],
+            eval_dataset=tokenized_dataset_dict['val'],
+        )
+
+        trainer.train(
+            resume_from_checkpoint=args.from_pretrained
+        )
+    else:
+        pass
+
+
+    
+
+
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,11 +1,17 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from transformers import GPT2LMHeadModel, GPT2Config, GPT2TokenizerFast, DataCollatorForLanguageModeling
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def init_model(context=None):
+    if context == 'bigram':
+        configuration = GPT2Config(n_positions=2)
+    else:
+        configuration = GPT2Config()
+    model = GPT2LMHeadModel(configuration)
+    return model
 
 def load_pretrained_model(pretrained_model_name_or_path):
     model = GPT2LMHeadModel.from_pretrained(pretrained_model_name_or_path)
-    model.to(device)
-    model.eval()
     return model
 
 def load_pretrained_tokenizer(pretrained_model_name_or_path, context=None):
@@ -21,7 +27,89 @@ def load_pretrained_tokenizer(pretrained_model_name_or_path, context=None):
     tokenizer.pad_token = tokenizer.eos_token # ?
     return tokenizer
 
+class ReverseSequenceDataCollator(DataCollatorForLanguageModeling):
+    def __call__(self, features, return_tensors=None):
+        for feature in features:
+            feature['input_ids'] = feature['input_ids'][::-1]
+        return super().__call__(features, return_tensors)
+
+class BidirectionalInfillingDataCollator(DataCollatorForLanguageModeling):
+    """
+    Modifies the DataCollatorForLanguageModeling to return
+    input_ids, labels, and attention_ids
+    as per pqian11/fragment-completion code (Qian and Levy, 2022)
+    suitable for bidirectional infilling task
+
+    From a single input (token) sentence, I should be able to create a whole batch
+    of bidirectional task inputs where each successive token is masked.
+    """
+    def __call__(self, features, return_tensors='pt', 
+                 BLANK_id=-2000, SEP_id=-1000, FILLER_id=-3000):
+        """
+        Given:
+            features := Dict{
+                'input_ids': List, 
+                'attention_mask': List
+            }
+        Returns:
+            batch := transformers.tokenization_utils_base.BatchEncoding{
+                'input_ids': Tensor,
+                'attention_mask': Tensor,
+                'labels': Tensor
+            }
+
+        Example:
+            Given:
+                input_ids = [1544, 7224, 1243, 845, 2092, 764]
+                attention_mask = [1, 1, 1, 1, 1, 1]
+
+            Return:
+                bidi_input_ids = [1544, BLANK, 1243, 845, 2092, 764, SEP, FILL]
+                bidi_attention_mask = [1, 1, 1, 1, 1, 1, 1, 1]
+                bidi_labels = [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, 7224]
+
+            * Ensure automatic shifting of labels doesn't happen in the model
+
+            (Note: Padding not necessary if we make the batch from the single sentence
+            Otherwise, we need to additionally pad all inputs with zeros at the end)
+
+        """
+        assert isinstance(features, dict), f"bidirectional data collator input features should be a dict, not {type(features)}"
+        assert return_tensors == 'pt', f"only supports return pytorch tensors"
+
+        feature = features
+        input_ids = feature['input_ids']
+
+        n_tokens = len(input_ids)
+
+        bidi_input_ids = [input_ids[:i] + [BLANK_id] + input_ids[i+1:] + [SEP_id, FILLER_id] 
+                        for i in range(n_tokens)]
+
+        bidi_attention_mask = [[1 for _ in range(n_tokens + 2)] for _ in range(n_tokens)]
+
+        bidi_labels = [[-100 for _ in range(n_tokens + 1)] + [answer_token] 
+                    for answer_token in input_ids]
+
+        batch = {
+            'input_ids': torch.tensor(bidi_input_ids),
+            'attention_mask': torch.tensor(bidi_attention_mask),
+            'labels': torch.tensor(bidi_labels)
+        }
+        # print(batch)
+        return batch
+        # return super().__call__(features, return_tensors)
+
+def init_data_collator(context_direction='left'):
+    if context_direction == 'left':
+        data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    elif context_direction == 'right':
+        data_collator = ReverseSequenceDataCollator(tokenizer, mlm=False)
+    elif context_direction == 'bidi':
+        data_collator = BidirectionalInfillingDataCollator(tokenizer, mlm=False)
+    return data_collator
+
 if __name__ == "__main__":
     model = load_pretrained_model('gpt2')
     tokenizer = load_pretrained_tokenizer('gpt2')
+    data_collator = init_data_collator('left')
 
