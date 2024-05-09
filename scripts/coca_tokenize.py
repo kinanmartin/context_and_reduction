e67d7f5d67438feabfb41154b052cb7112984949
Kinan Martin <kinanmartin0@gmail.com>
2024-05-09 14:32:45 -0400

    add word_ids to tokenize output

--- scripts/coca_tokenize.py
+++ scripts/coca_tokenize.py
@@ -16,19 +16,47 @@ def load_data_in_splits(data_dir, train=0.8, val=0.1, test=0.1, seed=41):
     print('...done')
     return out
 
-def tokenize_data(dataset_dict, tokenizer, context_size):
+def tokenize_data(dataset_dict, tokenizer, context_size, batch_size=1000):
     print('Tokenizing dataset_dict with tokenizer...')
 
-    def tokenize_func(x):
+    # def tokenize_func(x):
+    #     out = tokenizer(
+    #         ['[BOS] ' + text + ' [EOS]' for text in x['text']] if context_size == 'sentence' else x['text'],
+    #         truncation=True,
+    #         )
+    #     return out
+    
+    def tokenize_func(examples):
+        """
+        Splits text on whitespace before tokenizing, uses is_split_into_words=True.
+        datasets.map will treat the BatchEncoding output as a dict, and so we will lose the word/token mapping information.
+        Here, we save the Fast tokenizer word/token mappings by adding them as another key:value in out before returning
+        """
+        if context_size == 'sentence':
+            split_text = [['[BOS]'] + text.split(' ') + ['[EOS]'] for text in examples['text']]
+        else:
+            split_text = [text.split(' ') for text in examples['text']]
+
         out = tokenizer(
-            ['[BOS] ' + text + ' [EOS]' for text in x['text']] if context_size == 'sentence' else x['text'],
+            split_text,
             truncation=True,
+            is_split_into_words=True,
             )
+
+        out['word_ids'] = [out.word_ids(i) for i in range(len(out['input_ids']))]
+        # # Sanity check:
+        # for i in range(3):
+        #     print(examples['text'][i])
+        #     print(out['input_ids'][i])
+        #     print(out['word_ids'][i])
+        # assert False
         return out
 
     encoded_datasets = dataset_dict.map(
         tokenize_func, 
-        batched=True)
+        batched=True,
+        batch_size=batch_size,
+        writer_batch_size=batch_size)
     print('...done')
     return encoded_datasets
 
@@ -45,7 +73,8 @@ if __name__ == '__main__':
     parser.add_argument("--text_dir", type=str, default=None)
     parser.add_argument("--tokens_dir", type=str)
     parser.add_argument("--context_size", default='sentence')
-    parser.add_argument("--only_load", type=bool, default=False)
+    parser.add_argument("--only_load", type=str, default='n')
+    parser.add_argument("--batch_size", type=int, default=1000)
 
     args = parser.parse_args()
 
@@ -53,6 +82,7 @@ if __name__ == '__main__':
     output_dir = args.tokens_dir
     context_size = args.context_size
     only_load = args.only_load
+    batch_size = args.batch_size
 
 
     # tokenizer = load_pretrained_tokenizer('gpt2')
@@ -61,7 +91,8 @@ if __name__ == '__main__':
         context_size=context_size,
         context_direction=None,
         # adding prefix space will mess up [BOS] token
-        add_prefix_space=False if context_size == 'sentence' else True,
+        # add_prefix_space=False if context_size == 'sentence' else True,
+        add_prefix_space=True,
         padding=False,
         )
 
@@ -70,7 +101,7 @@ if __name__ == '__main__':
 
     # coca_dir = "data/coca_spoken_detokenized/text_sentence/"
 
-    if only_load:
+    if only_load == 'y':
         # Load pretokenized data:
         print('loading pretokenized data')
         encoded_datasets = load_from_disk(output_dir)
@@ -78,7 +109,7 @@ if __name__ == '__main__':
     else:
         coca_dsdict = load_data_in_splits(input_dir, .8, .1, .1)
         # Tokenize from data and save:
-        encoded_datasets = tokenize_data(coca_dsdict, tokenizer, context_size)
+        encoded_datasets = tokenize_data(coca_dsdict, tokenizer, context_size, batch_size)
 
         encoded_datasets.save_to_disk(output_dir)
 
@@ -89,6 +120,12 @@ if __name__ == '__main__':
     tokenized_datasets = encoded_datasets.remove_columns(['text'])
 
     print(encoded_datasets['train'][0:11])
+    print(type(encoded_datasets['train'][0]))
+
+    print(tokenizer(encoded_datasets['train'][0]['text'], truncation=True))
+    print(type(tokenizer(encoded_datasets['train'][0]['text'], truncation=True)))
+    print()
+    # print(encoded_datasets['train'][0].words())
 
     # import random
 
